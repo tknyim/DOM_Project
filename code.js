19b281f250f7e1c607bdde077eb7571db1c033b8
Eric Yim <eric.tk.yim@gmail.com>
2020-06-25 14:26:08 -0400

    Done

---
+++ code.js
@@ -0,0 +1,7 @@
+const gameHolder = document.getElementById("game");
+console.log(gameHolder);
+
+//Create a card function
+// Add a single card to the page
+// Add eventListener to the card *add eventlistener to a div/element* and have it do something
+// Use the function to make several cards
