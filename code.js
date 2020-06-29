16837f7adf46db9fcc2e790c611578ca13271eb8
Eric Yim <eric.tk.yim@gmail.com>
2020-06-29 03:14:24 -0400

    .

--- code.js
+++ code.js
@@ -1,62 +1,109 @@
-//Create a card function
-// Add a single card to the page
-// Add eventListener to the card *add eventlistener to a div/element* and have it do something
-// Use the function to make several cards
+// let section = document.getElementsByClassName("memory-game");
+// function newDivc(){
+//     let newDiv = document.createElement('div');
+//     newDiv.setAttribute("class","memory-cards");
+//     section.appendChild(newDiv);
+// };
+// function frontImageCard(){
+//     for (let i = 1; i < 10; i++) {
+//         var img = document.createElement("img");
+//         img.src = "starting_over/card_img/ff7_01" + i;
+//         newDivc.appendChild(img);
+//     };
+// };
 
-// need functions for:
-
-// card faced down
-// card faced up
-// reset
-// card clicked
-// create card
-// my cards
-
-const gameHolder = document.getElementById("game");
-selected = null;
-// points = 0;
+const cards = document.querySelectorAll(".memory-card");
+let cardFlipped = false;
+let waitCard = false;
+let firstCard, secondCard;
+let countMoves = document.getElementById("moves");
+let moves = 18;
+let matched = 0;
 
 
+// Card Flip
+function cardFlip(){
+    if (waitCard) return;
+    if(this === firstCard) return;
 
-// Card Dictionary [1]
-let cards = [["card1","ff1","ff1.png"], ["card2","ff2","ff2.png"], ["card3","ff3","ff3.png"], ["card4","ff4","ff4.png"], ["card5","ff5","ff5.png"], ["card6","ff6","ff6.png"], ["card7","ff7","ff7.png"], ["card8","ff8","ff8.png"], ["card9","ff9","ff9.png"], ["card10","ff10","ff10.png"], ["card11","ff11","ff11.png"], ["card12","ff12","ff12.png"], ["card13","ff13","ff13.png"], ["card14","ff15","ff15.png"], ["card15","fft","fft.png"]].map(cardArray =>{
-    return {
-        id : cardArray[0], //
-        node: createC(cardArray[0]), // ---- [2]
-        sideShown: "front", //
-        value: cardArray[1], //
-        faceImage: cardArray[2] // --------- "cards" is the dictionary for the game, '[0],[1],[2]' are reading the array pertaining in each param. 
+    this.classList.add("flip");
+
+    if(!cardFlipped){
+        // first card flipped
+        cardFlipped = true;
+        firstCard = this;
+        return;
     };
-});
-cards.forEach(card=gameHolder.append(card.node));
-//
-// Creating the Cards [2]
-const createC = (id) =>{
-    let findDiv = document.getElementById("deck");
-    let newCard = document.createElement("div");
-    newCard.setAttribute("id", id);
-    newCard.className = "card";
-    newCard.addEventListener(("click", selectC)); // ---- [3]
-    findDiv.append(newCard); // ----------- created a new element,"div id=card", under an existing element, "div id=deck"
+    // second card flipped
+    secondCard = this;
+    checkMatch();
+};
+// Check Match = True or False
+function checkMatch(){
+    let check = firstCard.dataset.framework === secondCard.dataset.framework;
+    check ? matchCard() : notMatch();
+};
+// Card Match = True
+function matchCard(){
+    firstCard.removeEventListener("click", cardFlip);
+    secondCard.removeEventListener("click", cardFlip);
+    firstCard.classList.add("match");
+    secondCard.classList.add("match");
+    matched ++;
+    let didMatch = document.getElementById('points');
+    didMatch.textContent = `Match: ${matched}`;
+    moves --;
+    let displayMoves = document.getElementById('moves');
+    displayMoves.textContent = `Move(s): ${moves}`;
+    resetCards();
+};
+// Mismatch = False
+function notMatch(){
+    waitCard = true;
+    setTimeout(() =>{
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+        moves --;
+        let displayMoves = document.getElementById('moves');
+        displayMoves.textContent = `Move(s): ${moves}`;
+        console.log(moves);
+
+        resetCards();
+    }, 1500);
+};
+// Resetting the Cards
+function resetCards(){
+    [cardFlipped, waitCard] = [false, false];
+    [firstCard, secondCard] = [null, null]
+};
+// Randomizer
+(function shuffleCards(){
+    cards.forEach(card =>{
+        let ranDom = Math.floor(Math.random() * 16); // dimensions
+        card.style.order = ranDom;
+    });
+})(); // "()" this function will execute right after it's def. // IIFE 
+
+// Final Results [NOT WORKING]
+function popUp(){
+    if (moves <= 0 || matched == 8){
+        let popScore = document.getElementsByClassName("popup");
+        popScore.classList.add("show");
+        document.getElementById("finalScore").innerHTML = matched;
+        document.getElementById("result").innerHTML = tallyPoints(matched);
+    };
+};
+function tallyPoints(){
+    if (matched.length == 8){
+        console.log('Perfect!');
+    } else if (matched.length <= 7 && matched.length >= 5){
+        console.log('Good job!');
+    } else if (matched.length <= 4 && matched.length > 3){
+        console.log('Nice try!');
+    } else {
+        console.log('Better luck next time.');
+    };
 };
 //
-// Selecting Cards [3]
-const selectC = (evt)=>{
-    let cardNode = evt.target;
-    let card = cards.find(card=>card.id == cardNode.id); // ------ [1]"cards" / [2]".id"
-    faceUp(card);
-    if ( !selected){
-        selected = card;
-    } else if (selected.value == card.value){
-        console.log("Match!")
-        window.setTimeout(()=>{                                     //
-            card.node.parentNode.removeChild(card.node);            //
-            selected.node.parentNode.removeChild(selected.node);    //
-            reset();                                                // 
-        }, 3000);                                                   //
-    } else {                                                        //
-        console.log("Not a Match.");                                //
-        window.setTimeout(reset, 3000);                             // ---- a timer for the message to console "match/not a match", 3000ms
-    };
-};
-//
+
+cards.forEach(card => card.addEventListener("click", cardFlip));
